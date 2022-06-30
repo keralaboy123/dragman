4fbfd27f4f5842e90acea37eebdcfb741165d923
keralaboy <85830575+keralaboy123@users.noreply.github.com>
2022-06-30 22:20:49 +0530

    Update mapman.py

--- mapman.py
+++ mapman.py
@@ -1,4 +1,5 @@
 from  dragman  import  DragEventManager,pynput
+import mouse
 
 emu = pynput.mouse.Controller()
 
@@ -16,7 +17,8 @@ class maper (DragEventManager):
 
         def  __init__(self):
             super().__init__()
-
+ 
+           
         def   shouldskip(self  ):          
               if   self.skipque :
                    self.skipque.pop()
@@ -32,15 +34,17 @@ class maper (DragEventManager):
                
             
         def on_click( self, x,y, button, buttonpressing  ):
-             if   self.shouldskip(): # these clicks are emulated echo  so skip it.
-                     self.listener.suppress_event()
-                     return True
+             
                 
              if  button == pynput.mouse.Button.left:
+                      
+                      return 
+
+             if   self.shouldskip(): # these clicks are emulated echo  so skip it.
+                     self.listener.suppress_event()
+                     return True             
                      
-                     return 
-                     
-             elif  button == pynput.mouse.Button.right:
+             if  button == pynput.mouse.Button.right:
 
                      print ("onclick of   of  maper") 
                      super().on_click( x,y, button, buttonpressing )
@@ -49,25 +53,27 @@ class maper (DragEventManager):
                              print(" button released on maper checking for  lastdrag event")
 
                              if self.isDragedLast():
-                                     print("there was a drag so this button release have nothing to handle")
-                                     pass
+                                     #print("there was a drag so this button release have nothing to handle")
+                                     self.listener.suppress_event()  # just  stop event here
+                                     
 
                              else:                            
                                       print("no drag happened  its a right  clickRelease so emulating   RIGHT button")
                                       
                                       self.skipque.append( pynput.mouse.Button.right)
                                       self.skipque.append( pynput.mouse.Button.right)
-                                      emu.click(pynput.mouse.Button.right)   # if there was no mouse movement between press and release then it was jus a click
+                                      
+                                      # if there was no mouse movement between press and release then it was jus a click
                                      
                                       
                              
                                                            
                      
-             self.listener.suppress_event()  # just  stop event here
-                                
+             
+                       
         def  on_dragEvent(self):                           
                     self.draged = True                    
-                    emu.scroll(0, -4)
+                    emu.scroll(0, 0.03)
                     
                     
 
